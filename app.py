5741887020283e5bbd2e59e4ca7479c6ddd77003
MaGaMe19 <mattia.metzler@gmail.com>
2021-05-11 23:48:55 +0200

    Ausprobieren

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import api_utils;
+from datetime import datetime as dt
+from random import randint
+
+api = api_utils.API()
+
+@api.GET("/api")
+def hello_world(request):
+    return f"   ---   Hello World   ---   Random number: {randint(1, 100)}   ---   Time: {dt.now().hour}:{dt.now().minute}:{dt.now().second}   ---   "
+
+api_utils.run(api)
