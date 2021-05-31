5c32e7d6d2d54a67c3f470b0202284837837eb78
MaGaMe19 <mattia.metzler@gmail.com>
2021-05-31 22:28:58 +0200

    - Nachrichten haben nun headers: "from" und "to" - die Nachrichten werden in diesem format gespeichert sowie auf dem front-end angezeigt. - mehr dokumentation

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import os
 from uuid import uuid4
 
 api = api_utils.API()
+
+# files
 filename = "data.json"
 filenameUsers = "users.json"
 
@@ -12,40 +14,58 @@ if not os.path.exists(filename):
     with open(filename, "w") as f:
         json.dump([], f)
 
+# prepare users.json if it doesn't exist
 if not os.path.exists(filenameUsers):
     with open(filenameUsers, "w") as f:
         json.dump({}, f)
 
 @api.GET("/api/")
 def get(request):
-    # get current list and send to webpage
+    # get current userlist to look up names
+    with open (filenameUsers) as f:
+        userList = json.load(f)
+    # get current datalist
     with open(filename) as f:
-        entryList = json.load(f)
-    return entryList
+        dataList = json.load(f)
+        messageList = [] # construct separate list for formatted messages
+        for listEntry in dataList:
+            # look up names in userList and format message accordingly
+            messageList.append(f'{userList[listEntry["from"]]} -> {userList[listEntry["to"]]}: {listEntry["message"]}')
+    return messageList
 
 @api.POST("/api/")
-def post(request, text:str, name:str):
-    # get current list and add new element with username
+def post(request, text:str, fromUser:str, toUser:str):
+    # get current list and add new element
     with open(filename) as f:
         entryList = json.load(f)
-    entryList.append(f"{name}: {text}")
+    # construct message with headers
+    entryList.append(
+        {
+            "from": fromUser,
+            "to": toUser,
+            "message": text
+        }
+    )
 
-    # dump new list into data.json
+    # dump updated list into data.json
     with open("data.json", "w") as f:
         json.dump(entryList, f, indent=4)
 
     return None
 
+# collect users
 @api.POST("/api/names/")
 def saveUser(request, name:str):
-    newUuid = str(uuid4())
+    newUuid = str(uuid4()) # create uuid (Universal Unique IDentification)
     with open(filenameUsers) as f:
         userList = json.load(f)
+        # save username as value with the uuid as key
         userList[newUuid] = name
 
     with open(filenameUsers, "w") as f:
         json.dump(userList, f, indent=4)
     
+    # return uuid that it can be stored locally in the browser
     return newUuid
 
 # clear list
